8689faa8a4aca4c646f2eaad5b63e4f0abf0e6e8
Oscar Roman <oscarroman.pe@gmail.com>
2021-12-18 11:17:08 -0500

    loader.js added

---
+++ js/loader.js
@@ -0,0 +1,9 @@
+(function(){
+
+window.addEventListener("load", function (){
+    var loader = document.querySelector(".loader");
+    loader.className += " oculto"; // class "loader oculto"
+});
+
+
+}())
